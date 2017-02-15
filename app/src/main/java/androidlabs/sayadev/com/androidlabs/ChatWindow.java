7b5a6612abcd028beecd456a04f517ef10bd8d0f
saleh <yass0016@gmail.com>
2017-02-15 14:02:21 -0500

    Updated Lab 5

--- app/src/main/java/androidlabs/sayadev/com/androidlabs/ChatWindow.java
+++ app/src/main/java/androidlabs/sayadev/com/androidlabs/ChatWindow.java
@@ -1,9 +1,13 @@
 package androidlabs.sayadev.com.androidlabs;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +23,21 @@ public class ChatWindow extends AppCompatActivity {
 
     final ArrayList<String> chatArray = new ArrayList<>();
 
+    private ChatDatabaseHelper dbHelper;
+    private SQLiteDatabase database;
+    private String[] allColumns = { ChatDatabaseHelper.COLUMN_ID,
+            ChatDatabaseHelper.COLUMN_MESSAGE };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+
+        dbHelper = new ChatDatabaseHelper(this);
+
+        database = dbHelper.getWritableDatabase();
+
+        readMessages();
 
         Resources resources = getResources();
         final ListView listViewChat = (ListView) findViewById(R.id.listViewChat);
@@ -36,10 +51,42 @@ public class ChatWindow extends AppCompatActivity {
             public void onClick(View v) {
                 String chatString = editTextChat.getText().toString();
                 chatArray.add(chatString);
+                writeMessages(chatString);
+
                 chatAdapter.notifyDataSetChanged();
                 editTextChat.setText("");
             }
         });
+    }
+
+    private void readMessages() {
+        // read database and save messages into the array list
+        Cursor cursor = database.query(ChatDatabaseHelper.TABLE_MESSAGES,
+                allColumns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            String message = cursor.getString(cursor.getColumnIndex( ChatDatabaseHelper.COLUMN_MESSAGE));
+            Log.i("Chat Window", "SQL MESSAGE:" + cursor.getString( cursor.getColumnIndex(ChatDatabaseHelper.COLUMN_MESSAGE)));
+            chatArray.add(message);
+            cursor.moveToNext();
+        }
+
+        Log.i("Chat Window", "Cursor’s column count =" + cursor.getColumnCount());
+
+        for(int i = 0; i < cursor.getColumnCount(); i++) {
+            Log.i("Chat Window", "Column Name: " + cursor.getColumnName(i));
+        }
+
+        // close the cursor
+        cursor.close();
+    }
+
+    private void writeMessages(String message) {
+        ContentValues values = new ContentValues();
+        values.put(ChatDatabaseHelper.COLUMN_MESSAGE, message);
+        database.insert(ChatDatabaseHelper.TABLE_MESSAGES, null,
+                values);
     }
 
     private class ChatAdapter extends ArrayAdapter<String> {
@@ -69,5 +116,12 @@ public class ChatWindow extends AppCompatActivity {
             message.setText(getItem(position));
             return result;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        dbHelper.close();
     }
 }
